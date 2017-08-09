5dd09b76df639cc44e16df27b30ba7b495d9cc8e
mynewusernamethatiwillforget <30871286+mynewusernamethatiwillforget@users.noreply.github.com>
2017-08-09 18:34:22 +0300

    Update tb.js

--- tb.js
+++ tb.js
@@ -10,6 +10,6 @@ function create(htmlStr) {
 
 window.onload = function () {
 	
-	var fragment3 = create('<img src="https://cdn.shopify.com/s/files/1/1875/6267/files/safesite_09fb14bf-436a-4daf-9ad5-14d84f863602.png?13174056267503880139" alt="Safe, secured and verified website." class="text-center center-block" style="margin:29px 0px">');
+	var fragment3 = create('<img src="https://www.conversionpirate.com/safesite.jpg" alt="safe site" class="text-center center-block" style="margin:29px 0px">');
 	document.getElementsByClassName('main__header')[0].appendChild(fragment3);
 };
